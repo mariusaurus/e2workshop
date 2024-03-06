f2fa0b3ed181fb2de9234e632202f38f62604620
MarsuPila <22983240+MarsuPila@users.noreply.github.com>
2024-03-06 13:49:03 +0100

    first exerecise

--- envs/launch_container_e2mip.sh
+++ envs/launch_container_e2mip.sh
@@ -8,6 +8,10 @@ PORT=8526
 MOUNTS="-v /home/mkoch/nv_sw:/nv_sw:rw"
 MOUNTS="-v /home/mkoch/Documents/projects/e2workshop:/e2ws:rw ${MOUNTS}"
 MOUNTS="-v /home/mkoch/Documents/playground/e2mip:/playground:rw ${MOUNTS}"
+MOUNTS="-v ${HOME}/.cache:/root/.cache:rw ${MOUNTS}"
+# MOUNTS="-v ${HOME}:/root:rw ${MOUNTS}"
+
+WORKDIR=/e2ws
 
 docker run \
     --shm-size=1g \
@@ -16,5 +20,6 @@ docker run \
     --runtime nvidia \
     --rm \
     --publish ${PORT}:${PORT} \
+    --workdir ${WORKDIR} \
     ${MOUNTS} \
     -it ${CONTAINER}:${TAG} bash -c "$RUN_CMD"
