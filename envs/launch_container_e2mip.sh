67d0c60982ae393c5391127244869e72a15731de
MarsuPila <22983240+MarsuPila@users.noreply.github.com>
2024-04-12 11:06:55 +0200

    final touches

--- envs/launch_container_e2mip.sh
+++ envs/launch_container_e2mip.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# provide path to code, to data and choose a port number different from you colleagues
-E2WORKSHOPDIR=${HOME}/Documents/projects/e2workshop
-E2WS_DATA_PATH=/media/rachel/e2ws_data
-PORT=8526
+# provide path to code, to data, container and choose a port number different from you colleagues
+E2WORKSHOPDIR= # /absolute/path/to/workshop/directory/e2workshop
+E2WS_DATA_PATH= # /absolute/path/to/data/directory/e2ws_data
+CONTAINER= # name of container, e.g. nvcr.io/e5z5rmoykw68/e2_workshop/e2workshop
+PORT= # choose a port number which is not used by any of your colleagues, smth like 8765
 
 # do not change below
-CONTAINER=nvcr.io/e5z5rmoykw68/e2_workshop/e2workshop
-TAG=2401.03
+TAG=latest
 RUN_CMD='bash'
 
 MOUNTS="-v ${E2WORKSHOPDIR}:/e2ws:rw ${MOUNTS}"
